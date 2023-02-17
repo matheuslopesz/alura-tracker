d0dd994da5754186790da6d1fde0f3627b701a2f
Matheus Lopes <matheuslopes.developer@gmail.com>
2023-02-17 12:49:51 -0300

    create task form component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
  <main class="columns is-gapless is-multiline">
-  <div class="collumn is-one-quarter">
-
-  </div>
-  <div class="collumn is-three-quarter">
+  <div class="column is-one-quarter">
     <SideBar />
+  </div>
+  <div class="column is-three-quarter">
+    <TaskForm />
   </div>
  </main>
 </template>
@@ -12,11 +12,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SideBar from './components/SideBar.vue';
+import TaskForm from './components/TaskForm.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    SideBar
+    SideBar,
+    TaskForm,
   },
   
 });
