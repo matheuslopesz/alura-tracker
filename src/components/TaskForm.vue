8003bd24377df3c484cfda79750522d94b2eda46
Matheus Lopes <matheuslopes.developer@gmail.com>
2023-02-17 22:38:50 -0300

    extract logict to timer task component

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -5,23 +5,7 @@
         <input type="text" class="input" placeholder="Add task" />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-        <StopWatch :timeInSeconds="timeInSeconds" />
-          <button class="button" @click="startTimer">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="stopTimer">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+       <TimerTask />
       </div>
     </div>
   </div>
@@ -29,28 +13,12 @@
 
 <script>
 import { defineComponent } from "vue";
-import StopWatch from "./StopWatch.vue";
+import TimerTask from "./TimerTask.vue";
 
 export default defineComponent({
   name: "TaskForm",
   components: {
-    StopWatch,
-  },
-  data() {
-    return {
-      timeInSeconds: 0,
-      stopwatch: 0,
-    };
-  },
-  methods: {
-    startTimer() {
-      this.stopwatch = setInterval(() => {
-        this.timeInSeconds++;
-      }, 1000);
-    },
-    stopTimer() {
-      clearInterval(this.stopwatch);
-    },
-  },
+    TimerTask,
+  }
 });
 </script>
---
+++ src/components/TimerTask.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <StopWatch :timeInSeconds="timeInSeconds" />
+    <button class="button" @click="startTimer">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="stopTimer">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import StopWatch from "./StopWatch.vue";
+
+export default defineComponent({
+  name: "TimerTask",
+  components: {
+    StopWatch,
+  },
+  data() {
+    return {
+      timeInSeconds: 0,
+      stopwatch: 0,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.stopwatch = setInterval(() => {
+        this.timeInSeconds++;
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.stopwatch);
+    },
+  },
+});
+</script>
